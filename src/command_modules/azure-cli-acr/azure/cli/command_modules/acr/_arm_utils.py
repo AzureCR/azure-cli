9f3adfc4cd217623cd92120bede5da9d029ae088
djyou <adamydj@gmail.com>
2016-09-10 21:25:00 -0700

    Added "az acr" commands
    1. Commands include create/delete/show/list/update Azure container
    registries.
    2. Added SDK for Python to manage Azure container registries.
    3. Integrated repository catalog and tags commands.

---
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_arm_utils.py
@@ -0,0 +1,84 @@
+#---------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#---------------------------------------------------------------------------------------------
+
+from azure.cli.command_modules.acr.containerregistry.models import RegistryParameters
+
+from ._factory import get_arm_service_client
+
+from azure.cli.command_modules.acr.containerregistry import VERSION
+
+resource_type = 'Microsoft.ContainerRegistry/registries'
+
+def arm_get_registries_in_subscription():
+    '''Returns the list of container registries in the current subscription.
+    '''
+    client = get_arm_service_client()
+    filter_str = "resourceType eq '{}'".format(resource_type)
+    result = list(client.resources.list(filter=filter_str))
+
+    return [RegistryParameters(item.id, item.name, item.location, item.tags) for item in result]
+
+def arm_get_registries_in_resource_group(resource_group):
+    '''Returns the list of container registries in the resource group.
+    :param str resource_group: The name of resource group
+    '''
+    client = get_arm_service_client()
+    filter_str = "resourceType eq '{}'".format(resource_type)
+    result = list(client.resource_groups.list_resources(resource_group, filter=filter_str))
+
+    return [RegistryParameters(item.id, item.name, item.location, item.tags) for item in result]
+
+def arm_get_registry_by_name(registry_name):
+    '''Returns the container registry that matches the registry name.
+    :param str registry_name: The name of container registry
+    '''
+    registries = arm_get_registries_in_subscription()
+    elements = [item for item in registries if item.name.lower() == registry_name.lower()]
+
+    if len(elements) == 0:
+        return None
+    elif len(elements) == 1:
+        return elements[0]
+    else:
+        raise ValueError('More than one container registries are found with name: ' + registry_name)
+
+def arm_deploy_template(resource_group, registry_name, location, #pylint: disable=too-many-arguments
+                        storage_account_name, deployment_name, mode='incremental'):
+    '''Deploys ARM template to create a container registry.
+    :param str resource_group: The name of resource group
+    :param str registry_name: The name of container registry
+    :param str location: The name of location
+    :param str storage_account_name: The name of storage account
+    :param str deployment_name: The name of deployment
+    :param str mode: The mode of deployment
+    '''
+    from azure.mgmt.resource.resources.models import DeploymentProperties
+    from azure.cli.core._util import get_file_json
+    import os
+
+    file_path = os.path.join(os.path.dirname(__file__), 'template.json')
+    template = get_file_json(file_path)
+    parameters = _parameters(registry_name, location, storage_account_name)
+    properties = DeploymentProperties(template=template, parameters=parameters, mode=mode)
+
+    client = get_arm_service_client()
+
+    return client.deployments.create_or_update(resource_group, deployment_name, properties)
+
+def _parameters(registry_name, location, storage_account_name):
+    '''Returns a dict of deployment parameters.
+    :param str registry_name: The name of container registry
+    :param str location: The name of location
+    :param str storage_account_name: The name of storage account
+    '''
+    parameters = {
+        'registryName': {'value': registry_name},
+        'registryLocation': {'value': location},
+        'registryApiVersion': {'value': VERSION},
+        'storageAccountName': {'value': storage_account_name},
+        'storageAccountLocation': {'value': 'westus'},
+        'storageAccountApiVersion': {'value': '2015-05-01-preview'}
+    }
+    return parameters
---
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
@@ -0,0 +1,33 @@
+#---------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#---------------------------------------------------------------------------------------------
+
+from azure.cli.core.commands import (
+    register_cli_argument,
+    CliArgumentType
+)
+
+from azure.cli.core.commands.parameters import (
+    name_type,
+    resource_group_name_type,
+    location_type,
+    tags_type
+)
+
+storage_account_name_type = CliArgumentType(
+    options_list=('--storage-account-name', '-s'),
+    help='Name of storage account.'
+)
+
+storage_account_key_type = CliArgumentType(
+    options_list=('--storage-account-key', '-k'),
+    help='Key of storage account.'
+)
+
+register_cli_argument('acr', 'registry_name', arg_type=name_type)
+register_cli_argument('acr', 'resource_group', arg_type=resource_group_name_type)
+register_cli_argument('acr', 'location', arg_type=location_type)
+register_cli_argument('acr', 'tags', arg_type=tags_type)
+register_cli_argument('acr', 'storage_account_name', arg_type=storage_account_name_type)
+register_cli_argument('acr', 'storage_account_key', arg_type=storage_account_key_type)
