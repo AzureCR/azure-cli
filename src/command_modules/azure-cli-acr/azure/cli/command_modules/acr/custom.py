38e19de99a377b2ceedc6bfd5344bf063ddd6a2f
Dongjiang You <djyou@users.noreply.github.com>
2017-05-01 12:34:19 -0700

    [ACR] Update to 2017-06-01-preview version (#40)
    * [ACR] Update to 2017-06-01-preview version
    
    Add webhooks and replications as nested resources under registries.

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/custom.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/custom.py
@@ -5,7 +5,7 @@
 
 from azure.cli.core.commands import LongRunningOperation
 
-from azure.mgmt.containerregistry.models import (
+from .azure.mgmt.containerregistry.models import (
     RegistryUpdateParameters,
     StorageAccountParameters
 )
@@ -16,8 +16,8 @@ from ._utils import (
     get_access_key_by_storage_account_name,
     arm_deploy_template_new_storage,
     arm_deploy_template_existing_storage,
-    random_storage_account_name,
-    get_location_from_resource_group
+    arm_deploy_template_managed_storage,
+    random_storage_account_name
 )
 from ._docker_utils import docker_login_to_registry
 
@@ -63,31 +63,49 @@ def acr_create(registry_name,  # pylint: disable=too-many-arguments
     :param str admin_enabled: Indicates whether the admin user is enabled
     :param str deployment_name: The name of the deployment
     """
-    if location is None:
-        location = get_location_from_resource_group(resource_group_name)
     client = get_acr_service_client().registries
     admin_user_enabled = admin_enabled == 'true'
 
-    if storage_account_name is None:
-        storage_account_name = random_storage_account_name(registry_name)
-        LongRunningOperation()(
-            arm_deploy_template_new_storage(
-                resource_group_name,
-                registry_name,
-                location,
-                sku,
+    if sku == 'Basic':
+        if storage_account_name is None:
+            storage_account_name = random_storage_account_name(registry_name)
+            logger.warning(
+                "A new storage account '%s' will be created in resource group '%s'.",
                 storage_account_name,
-                admin_user_enabled,
-                deployment_name)
-        )
+                resource_group_name)
+            LongRunningOperation()(
+                arm_deploy_template_new_storage(
+                    resource_group_name,
+                    registry_name,
+                    location,
+                    sku,
+                    storage_account_name,
+                    admin_user_enabled,
+                    deployment_name)
+            )
+        else:
+            LongRunningOperation()(
+                arm_deploy_template_existing_storage(
+                    resource_group_name,
+                    registry_name,
+                    location,
+                    sku,
+                    storage_account_name,
+                    admin_user_enabled,
+                    deployment_name)
+            )
     else:
+        if storage_account_name:
+            logger.warning(
+                "'%s' SKU uses managed storage account. Storage account '%s' will be ignored.",
+                sku,
+                storage_account_name)
         LongRunningOperation()(
-            arm_deploy_template_existing_storage(
+            arm_deploy_template_managed_storage(
                 resource_group_name,
                 registry_name,
                 location,
                 sku,
-                storage_account_name,
                 admin_user_enabled,
                 deployment_name)
         )
@@ -110,9 +128,8 @@ def acr_delete(registry_name, resource_group_name=None):
     :param str registry_name: The name of container registry
     :param str resource_group_name: The name of resource group
     """
-    if resource_group_name is None:
-        resource_group_name = get_resource_group_name_by_registry_name(registry_name)
-
+    resource_group_name = get_resource_group_name_by_registry_name(
+        registry_name, resource_group_name)
     client = get_acr_service_client().registries
 
     return client.delete(resource_group_name, registry_name)
@@ -123,9 +140,8 @@ def acr_show(registry_name, resource_group_name=None):
     :param str registry_name: The name of container registry
     :param str resource_group_name: The name of resource group
     """
-    if resource_group_name is None:
-        resource_group_name = get_resource_group_name_by_registry_name(registry_name)
-
+    resource_group_name = get_resource_group_name_by_registry_name(
+        registry_name, resource_group_name)
     client = get_acr_service_client().registries
 
     return client.get(resource_group_name, registry_name)
@@ -134,9 +150,8 @@ def acr_show(registry_name, resource_group_name=None):
 def acr_update_get(client,
                    registry_name,
                    resource_group_name=None):
-    if resource_group_name is None:
-        resource_group_name = get_resource_group_name_by_registry_name(registry_name)
-
+    resource_group_name = get_resource_group_name_by_registry_name(
+        registry_name, resource_group_name)
     props = client.get(resource_group_name, registry_name)
 
     return RegistryUpdateParameters(
@@ -172,8 +187,8 @@ def acr_update_set(client,
                    registry_name,
                    resource_group_name=None,
                    parameters=None):
-    if resource_group_name is None:
-        resource_group_name = get_resource_group_name_by_registry_name(registry_name)
+    resource_group_name = get_resource_group_name_by_registry_name(
+        registry_name, resource_group_name)
 
     return client.update(resource_group_name, registry_name, parameters)
 
