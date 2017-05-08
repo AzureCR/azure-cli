c223a4c27ed9c2fb081616a300b4b3ea92ef9728
Dongjiang You <djyou@users.noreply.github.com>
2017-05-08 08:55:43 -0700

    Various improvements and bug/style fixes (#41)
    * Various improvements and bug/style fixes
    
    * Fix bool parameters

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
@@ -22,11 +22,7 @@ from ._constants import (
     WEBHOOK_RESOURCE_TYPE,
     REPLICATION_RESOURCE_TYPE
 )
-from ._validators import (
-    validate_registry_name,
-    validate_headers,
-    validate_replication_location
-)
+from ._validators import validate_registry_name, validate_headers
 
 register_cli_argument('acr', 'registry_name', options_list=('--name', '-n'), help='The name of the container registry', completer=get_resource_name_completion_list(ACR_RESOURCE_TYPE))
 register_cli_argument('acr', 'storage_account_name', help='Provide the name of an existing storage account if you\'re recreating a container registry over a previous registry created storage account. Only applicable to Basic SKU.', completer=get_resource_name_completion_list(STORAGE_RESOURCE_TYPE))
@@ -52,10 +48,9 @@ register_cli_argument('acr webhook', 'webhook_name', options_list=('--name', '-n
 register_cli_argument('acr webhook', 'uri', help='The service URI for the webhook to post notifications.')
 register_cli_argument('acr webhook', 'headers', nargs='*', help="Space separated custom headers in 'key[=value]' format that will be added to the webhook notifications. Use {} to clear existing headers.".format(quotes), validator=validate_headers)
 register_cli_argument('acr webhook', 'actions', nargs='+', help='Space separated list of actions that trigger the webhook to post notifications.', choices=['push', 'pull', 'delete'])
-register_cli_argument('acr webhook', 'is_enabled', help='Indicates whether the webhook is enabled.', choices=['true', 'false'])
+register_cli_argument('acr webhook', 'is_enabled', nargs=1, help='Indicates whether the webhook is enabled.', choices=['true', 'false'])
 register_cli_argument('acr webhook create', 'webhook_name', completer=None)
 
 register_cli_argument('acr replication', 'registry_name', options_list=('--registry', '-r'), help='The name of the container registry', completer=get_resource_name_completion_list(ACR_RESOURCE_TYPE))
 register_cli_argument('acr replication', 'replication_name', options_list=('--name', '-n'), help='The name of the replication', completer=get_resource_name_completion_list(REPLICATION_RESOURCE_TYPE))
 register_cli_argument('acr replication create', 'replication_name', completer=None)
-register_cli_argument('acr replication create', 'location', validator=validate_replication_location)
