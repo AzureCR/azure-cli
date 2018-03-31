c086b79dbbcc3141d8abd50f0f7d6d52f0833a90
Eric Hotinger <erhoting@microsoft.com>
2018-03-30 17:17:13 -0700

    Remove CPU from client

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/build.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/build.py
@@ -244,7 +244,6 @@ def acr_queue(cmd,
 
     # hard-code platform to linux and cpu to 1
     platform = PlatformProperties("Linux")
-    platform.cpu = 1
 
     build_arguments = []
     if not (build_args is None):
