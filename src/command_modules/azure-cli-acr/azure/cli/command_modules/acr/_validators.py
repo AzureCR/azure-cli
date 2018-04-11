6f78ee54e86225e2961833bda0ee8a882afde3e1
nataliegong <nataliegong2012@gmail.com>
2018-04-11 15:11:17 -0700

    Validate build task name Alphanumeric

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_validators.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_validators.py
@@ -41,5 +41,6 @@ def validate_build_task_name(namespace):
     import re
     build_task_name = namespace.build_task_name
     p = re.compile('^[a-zA-Z0-9]*$')
-    if p.match(build_task_name) == None:
-        raise CLIError("Build task name may contain alpha numeric characters only and must be between 5 and 50 characters.")
+    if p.match(build_task_name) is None:
+        raise CLIError(
+            "Build task name may contain alpha numeric characters only and must be between 5 and 50 characters.")
