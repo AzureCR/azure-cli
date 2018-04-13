dbe56f87f3a2cce33117161134ffd4f4e08c6b02
Dongjiang You <doyou@microsoft.com>
2018-04-13 11:09:46 -0700

    Add logging if failed to parse datetime

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_format.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_format.py
@@ -4,6 +4,9 @@
 # --------------------------------------------------------------------------------------------
 
 from collections import OrderedDict
+from knack.log import get_logger
+
+logger = get_logger(__name__)
 
 
 def registry_output_format(result):
@@ -131,8 +134,8 @@ def _build_format_group(item):
         ('TASK', _get_value(item, 'buildTask')),
         ('PLATFORM', _get_value(item, 'platform', 'osType')),
         ('STATUS', _get_value(item, 'status')),
-        ('STARTED', _format_datetime(_get_value(item, 'status'))),
-        ('DURATION', _get_duration(_get_value(item, 'finishTime'), _get_value(item, 'startTime')))
+        ('STARTED', _format_datetime(_get_value(item, 'startTime'))),
+        ('DURATION', _get_duration(_get_value(item, 'startTime'), _get_value(item, 'finishTime')))
     ])
 
 
@@ -153,10 +156,11 @@ def _format_datetime(date_string):
     try:
         return parse(date_string).strftime("%Y-%m-%dT%H:%M:%SZ")
     except ValueError:
+        logger.debug("Unable to parse date_string '%s'", date_string)
         return date_string or ' '
 
 
-def _get_duration(finish_time, start_time):
+def _get_duration(start_time, finish_time):
     from dateutil.parser import parse
     try:
         duration = parse(finish_time) - parse(start_time)
@@ -165,4 +169,5 @@ def _get_duration(finish_time, start_time):
         seconds = "{0:02d}".format(duration.seconds % 60)
         return "{0}:{1}:{2}".format(hours, minutes, seconds)
     except ValueError:
+        logger.debug("Unable to get duration with start_time '%s' and finish_time '%s'", start_time, finish_time)
         return ' '
