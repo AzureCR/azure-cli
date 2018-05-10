99a2b83bfdf8d645d4097d5f593fac662746b643
nataliegong <nataliegong2012@gmail.com>
2018-05-10 16:59:13 -0700

    Enable image import commands

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/image.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/image.py
@@ -3,24 +3,33 @@ from azure.mgmt.containerregistry.v2018_02_01_preview.models import (
     ImportImageParameters,
     ImportSource
 )
-from ._utils import validate_managed_registry
+from ._utils import (
+    validate_managed_registry,
+    get_resource_id_by_registry_name
+)
+
 from ._client_factory import cf_acr_registries
 
 IMPORT_NOT_SUPPORTED = "Image imports are only supported for managed registries."
 
-
 def acr_image_import(cmd,
                      registry_name,
-                     resource_id,
-                     source_image,
-                     target_tags,
-                     untagged_target_repositories=None,
+                     image=None,
+                     resource_id=None,
+                     source_image=None,
+                     target_tags=None,
                      resource_group_name=None,
+                     untagged_target_repositories=None,
                      mode="NoForce"):
-    _, resource_group_name = validate_managed_registry(
+    registry, resource_group_name = validate_managed_registry(
         cmd.cli_ctx, registry_name, resource_group_name, IMPORT_NOT_SUPPORTED)
 
     client_registries = cf_acr_registries(cmd.cli_ctx)
+
+    if image is not None:
+        source_registry = image[:image.index(".")]
+        resource_id = get_resource_id_by_registry_name(cmd.cli_ctx, source_registry)
+        source_image = image[image.index("/") + 1 :]
 
     image_source = ImportSource(
         resource_id=resource_id, source_image=source_image)
