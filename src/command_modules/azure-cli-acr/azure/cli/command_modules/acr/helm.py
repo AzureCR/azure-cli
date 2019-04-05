e5996a9b3718b4cb9e4451b828e4824e35a98610
Dongjiang You <doyou@microsoft.com>
2019-04-05 15:23:46 -0700

    Fix helm

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/helm.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/helm.py
@@ -119,7 +119,7 @@ def acr_helm_push(cmd,
         username=username,
         password=password,
         artifact_repository=repository,
-        permission='push')
+        permission='push,pull')
 
     path = _get_blobs_path(repository, basename(chart_package))
 
